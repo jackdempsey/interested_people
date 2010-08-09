221a730bcb8a5f4f8b72f3855465d93d0b3cebb0
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-09 18:55:33 -0400

    you have to generate migration yourself for now

--- lib/generators/interested_people/install_generator.rb
+++ lib/generators/interested_people/install_generator.rb
@@ -1,6 +1,7 @@
 module InterestedPeople
   module Generators
     class InstallGenerator < Rails::Generators::Base
+
       source_root File.expand_path("../templates", __FILE__)
 
       desc "Install interested people files into your app"
@@ -9,9 +10,9 @@ module InterestedPeople
         copy_file "en.yml", "config/locales/interested_people.en.yml"
       end
 
-      #def copy_migration
-      #  migration_template "migration.rb", "db/migrate/create_interested_people"
-      #end
+      def print_migration
+        readme "migration.rb"
+      end
 
     end
   end
--- lib/generators/interested_people/templates/migration.rb
+++ lib/generators/interested_people/templates/migration.rb
@@ -1,3 +1,6 @@
+# Copy the up and down methods below into a migration you generate on your own
+# When ActiveRecord::Generators::Base doesn't inherit from NamedBase 
+# I'll change this to generate the migration for you as well
 class InterestedPeopleCreateTable < ActiveRecord::Migration
   def self.up
     create_table(:interested_people) do |t|
