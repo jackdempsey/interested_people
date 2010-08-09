7a8583d7bfac8749ce5a364f471ab5fe2789feb0
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-09 15:39:15 -0400

    create a simple create action, while allowing myself to introduce myself

--- app/controllers/interested_people_controller.rb
+++ app/controllers/interested_people_controller.rb
@@ -2,4 +2,12 @@ class InterestedPeopleController < ApplicationController
 
   unloadable
 
+  def create
+    interested_person = InterestedPerson.new(params[:interested_person])
+    if interested_person.save
+      flash[:notice] = t 'interested_person.create.success'
+    else
+      render :new
+    end
+  end
 end
