13bb1a080edc1d651a95fff858f03b6459f5a31d
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-09 18:55:55 -0400

    need to redirect somewhere. later on let this be configured

--- app/controllers/interested_people_controller.rb
+++ app/controllers/interested_people_controller.rb
@@ -6,6 +6,7 @@ class InterestedPeopleController < ApplicationController
     interested_person = InterestedPerson.new(params[:interested_person])
     if interested_person.save
       flash[:notice] = t 'interested_person.create.success'
+      redirect_to '/'
     else
       render :new
     end
