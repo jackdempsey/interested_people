590587ef53556b985c0baf1b1d3026070e2066c0
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-09 15:52:43 -0400

    create a migration for end user

---
+++ lib/generators/templates/migration.rb
@@ -0,0 +1,16 @@
+class InterestedPeopleCreateTable < ActiveRecord::Migration
+  def self.up
+    create_table(:interested_people) do |t|
+      t.string :name, :email
+      t.boolean :contacted
+
+      t.timestamps
+    end
+
+    add_index :interested_people, :email,                :unique => true
+  end
+
+  def self.down
+    drop_table :interested_people
+  end
+end
