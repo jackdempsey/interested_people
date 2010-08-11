1931cd841a22a72b5ca5e333ac90d552842c31b0
Jack Dempsey <jack.dempsey@gmail.com>
2010-08-11 11:07:10 -0400

    cleanup a bit. still ugly, but less ugly. hopefully.

--- app/controllers/interested_people_controller.rb
+++ app/controllers/interested_people_controller.rb
@@ -2,9 +2,13 @@ class InterestedPeopleController < ApplicationController
 
   unloadable
 
+  def new
+    @interested_person = InterestedPerson.new
+  end
+
   def create
-    interested_person = InterestedPerson.new(params[:interested_person])
-    if interested_person.save
+    @interested_person = InterestedPerson.new(params[:interested_person])
+    if @interested_person.save
       flash[:notice] = t 'interested_person.create.success'
       redirect_to '/'
     else
