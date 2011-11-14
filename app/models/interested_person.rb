2e432f1a63a1368375d718885b2411f010d91aa3
Jack Dempsey <jack.dempsey@gmail.com>
2011-11-14 11:12:13 -0800

    provide a scope to filter out emails that correspond to people already in your users table

--- app/models/interested_person.rb
+++ app/models/interested_person.rb
@@ -1,3 +1,4 @@
 class InterestedPerson < ActiveRecord::Base
   validates_uniqueness_of :email
+  scope :not_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is not null")
 end
