a8b98b336c1a01a222fc8974d0b34c7826c08a81
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-08 14:25:54 -0700

    setup basic home controller

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -48,7 +48,7 @@ Dummy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => lambda {|env| [200, {'Content-type' => 'text/html'}, ["Welcome to Dummy App"]]}
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
