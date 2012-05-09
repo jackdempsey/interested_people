4eb04bbd69cc134b75f72ac0e0185c84d5fb50a7
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-08 18:04:58 -0700

    comment out scopes for now--need to come back and fix table issue

--- app/models/interested_person.rb
+++ app/models/interested_person.rb
@@ -1,5 +1,5 @@
 class InterestedPerson < ActiveRecord::Base
   validates_uniqueness_of :email
-  scope :already_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is not null")
-  scope :not_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is null")
+  # scope :already_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is not null")
+  # scope :not_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is null")
 end
