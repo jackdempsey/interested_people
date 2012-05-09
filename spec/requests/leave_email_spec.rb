968624eedc11118a07a29d7726a6af98b2a7b830
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-08 18:15:43 -0700

    basic spec to ensure we can create a new record

--- spec/requests/leave_email_spec.rb
+++ spec/requests/leave_email_spec.rb
@@ -12,5 +12,8 @@ feature "User signs up for mailing list", %q{
   scenario "Load main page with form" do
     visit '/'
     page.should have_content('Welcome')
+
+    fill_in 'interested_person_email', with: 'jack.dempsey@me.com'
+    click_on 'Create Interested person'
   end
 end
