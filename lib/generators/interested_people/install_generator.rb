ffc8b66094fd7ed1dfbd54784511f47b267c45b2
Jack Dempsey <jack.dempsey@gmail.com>
2012-06-01 11:10:43 -0700

    tweak message

--- lib/generators/interested_people/install_generator.rb
+++ lib/generators/interested_people/install_generator.rb
@@ -4,7 +4,7 @@ module InterestedPeople
 
       source_root File.expand_path("../templates", __FILE__)
 
-      desc "Install interested people files into your app"
+      desc "Install InterestedPeople files into your app"
 
       def copy_locale
         copy_file "../../../../config/locales/en.yml", "config/locales/interested_people.en.yml"
