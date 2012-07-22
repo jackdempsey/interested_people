5da48bb9a096494ada54472050dc3406d0808cac
Jack Dempsey <jack.dempsey@gmail.com>
2012-07-22 15:23:12 -0700

    fixed conflict

--- app/models/interested_person.rb
+++ app/models/interested_person.rb
@@ -1,6 +1,7 @@
 class InterestedPerson < ActiveRecord::Base
   validates_uniqueness_of :email
-  attr_accessible :email
   # scope :already_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is not null")
   # scope :not_a_user, joins("left outer join users on users.email = interested_people.email").where("users.id is null")
+
+  attr_accessible :name, :email
 end
